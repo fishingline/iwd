48dc2ec8cd43379e6b15b22042a9d40306def55d
mattmc3 <mattmc3@gmail.com>
2021-11-26 14:51:52 -0500

    Change iwd to print like pwd

--- functions/iwd.fish
+++ functions/iwd.fish
@@ -1,3 +1,3 @@
-function iwd --description "cd to the initial working directory, set at the start of a session"
-    cd "$IWD"
+function iwd --description "Print the initial working directory"
+    echo "$IWD"
 end
